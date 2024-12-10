960b19053c8f1b37fac07bc817d1de515f943ea8
BastienTLC <bastientalec@orange.fr>
2024-12-10 10:45:45 +0100

    rename

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
     const handleAuth = () => {
-        window.location.href = `https://id.twitch.tv/oauth2/authorize?client_id=v5ow6jdcrqa6s1f5pmvezomn7pgwdm&redirect_uri=${backendUrl}/api/twitch/callback&response_type=code&scope=chat:read+chat:edit`;
+        window.location.href = `https://id.twitch.tv/oauth2/authorize?client_id=v5ow6jdcrqa6s1f5pmvezomn7pgwdm&redirect_uri=https://nameless-springs-65369-f87eb19f76ac.herokuapp.com/api/twitch/callback&response_type=code&scope=chat:read+chat:edit`;
     };
 
     return (
